a4f14d3ce108a91870fa9e0b4b2f13bcff3b920f
NIKHIL JAIN <nj30sep@gmail.com>
2026-08-25 14:19:26 +0530

    Refactor div function to lambda for divisibility check
    Refactored the div function to a lambda expression.

--- 13.  ADVANCE PYTHON 2/Q4.py
+++ 13.  ADVANCE PYTHON 2/Q4.py
@@ -3,11 +3,7 @@ l =[]
 for i in range (10):
     l.append(int(input("enter a number for the list: ")))
 
-def div(s):
-    if s%5==0:
-        return s
-    else:
-        return False   
+div = lambda x:x%5==0
 
 
 fl = filter (div,l) 
