f632de7858de8e184df4b9716b5f91500b87f133
NIKHIL JAIN <nj30sep@gmail.com>
2026-08-25 14:05:00 +0530

    Modify multiplication table output format
    Change table elements to strings and join for output

--- 13.  ADVANCE PYTHON 2/Q3.py
+++ 13.  ADVANCE PYTHON 2/Q3.py
@@ -1,6 +1,6 @@
 t  = int(input("enter a number: "))
 
-table =[t*i for i in range(1,11)]
-
-for i in table:
-    print(i)
+table =[str(t*i) for i in range(1,11)]
+# .join() — combining a list of strings into one string
+i = "\n".join(table) 
+print(i)
